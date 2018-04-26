fceef1c2c90e00debb7df949f1b993594813d387
TheCodeWorm <4purposeliving@gmail.com>
2018-04-26 12:11:25 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const person = {
+  name: ""
+}
+
+const PersonName = () => {
+  return person.name ? <h1>Name: {person.name}</h1> : 
+    <h1>Name: Unknown</h1>
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +22,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <PersonName />
       </div>
     );
   }
